72172c35daa8de9a29b1feffd12278e41cc449ba
Randy Tolentino <makertolentino@gmail.com>
2015-06-05 05:47:03 -0500

    testing pull of fb object

--- js/lib/controllers/listController.js
+++ js/lib/controllers/listController.js
@@ -1,5 +1,7 @@
-myApp.controller('ListController', function($scope) {
+myApp.controller('ListController', function($scope, $firebaseObject) {
 
-  
+  var ref = new Firebase('https://devironment.firebaseio.com');
+
+  $scope.data = $firebaseObject(ref);
 
 }); //ListController
