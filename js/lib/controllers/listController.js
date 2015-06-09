02e89b67ba3da3e8ae071c7a3ea5dec1e257fac7
Randy Tolentino <makertolentino@gmail.com>
2015-06-09 04:38:34 -0500

    transferring to vanilla

--- js/lib/controllers/listController.js
+++ js/lib/controllers/listController.js
@@ -1,7 +1,12 @@
-myApp.controller('ListController', function($scope, $firebaseObject) {
+myApp.controller('ListController', function($scope, $firebaseObject, $location, $anchorScroll) {
 
   var ref = new Firebase('https://devironment.firebaseio.com/resources');
 
   $scope.resources = $firebaseObject(ref);
+
+   $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   };
 
 }); //ListController
